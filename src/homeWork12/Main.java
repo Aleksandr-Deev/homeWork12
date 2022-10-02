5c7456d399b03c088cdf5962354ada73e9f04b08
Aleksandr Deev <geshtait@mail.ru>
2022-10-02 23:55:11 +0500

    Сделал ДЗ Объекты и Классы

--- src/homeWork12/Main.java
+++ src/homeWork12/Main.java
@@ -2,6 +2,12 @@ package homeWork12;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author alexanderPushkin = new Author("Alexander", "Pushkin");
+        Author sergeyYesenin = new Author("Sergey", "Yesenin");
+
+        Book warAndPeace = new Book(1863, alexanderPushkin);
+        Book rowanFire = new Book(1924, sergeyYesenin);
+
+        rowanFire.setYearOfPublication(1968);
     }
 }
